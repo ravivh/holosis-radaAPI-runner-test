c0105683e7d47b040aef72edde4dc4506731024d
Leo Katz <lkatz@Holosishealth.com>
2025-04-07 10:28:55 +0300

    Correction to cmake instructions

--- README.cmake
+++ README.cmake
@@ -4,5 +4,5 @@ Build with CMAKE
 mkdir -p Build
 rm -fr Build/*
 cd Build/
-cmake ../ -DCMAKE_TOOLCHAIN_FILE=../cmake/aarch64-linux-gnu.cmake -DIMX8="True"
+cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/aarch64-linux-gnu.cmake -DIMX8="True" ..
 make
